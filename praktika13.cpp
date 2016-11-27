8135a404bd277d36746a644dcd42088d2be28f35
Juliayuliya <yuliya11155@mail.ru>
2016-11-27 22:11:05 +0300

    Add files via upload

---
+++ praktika13.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <math.h>
+#include <iostream>
+#include <clocale>
+
+int main()
+{
+float PI,rez,data;
+char s[10];
+setlocale(LC_CTYPE, "rus");
+printf("введите длинну:");
+PI=3,14;
+scanf("%s",s);
+char c=s[strlen(s)-1];
+s[strlen(s)-1]='\0';
+data=atof(s);//converts a string to float
+switch (c)
+{
+case 'D': rez=data*PI/180;
+break;
+case 'R': rez=data*180/PI;
+break;
+default: printf("ошибка ввода");
+break;
+}
+printf("%.5f",rez);
+system("pause");
+return 0;
+}
