298d1018e5f181f6ff457852d6ce86b266fe3242
Juliayuliya <yuliya11155@mail.ru>
2016-11-27 21:46:04 +0300

    Add files via upload

---
+++ praktika14.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+int f,d;
+printf("vvedite rost v americanscoy sisteme(futy,dymy)");
+scanf("%d%d",&f,&d);
+double sm=(f*12+d)*2.54;
+printf("vash rost v evropeyscoy sisteme");
+printf("%.1f",sm);
+getch();
+return 0;
+}
